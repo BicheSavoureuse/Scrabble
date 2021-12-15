d7ef296a1f32d15567cc431ff5f2367fa6b245e0
Louis <Louis@DESKTOP-I7HDCMG>
2021-12-15 18:49:28 +0100

    Ajoutez des fichiers projet.

---
+++ Scrabble/Joueur.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Scrabble
+{
+    class Joueur
+    {
+        public class Info_Joueur
+        {
+            private string prenom;
+            private string nom;
+            private int score;
+            private string mots;
+
+        }
+        public void Add_Mot(string mot)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
